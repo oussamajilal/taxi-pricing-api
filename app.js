afddc038bf808bdcd10bbad35e7b9a1089d10813
Oussama Jilal <oussama@jilal.net>
2021-06-13 20:25:28 +0100

    Added support for OPTIONS http method

--- app.js
+++ app.js
@@ -15,9 +15,29 @@ server.on('restifyError', function (req, res, err, next) {
   res.json(err.errors);
 });
 
-router.configure(server);
+function unknownMethodHandler(req, res) {
+  if (req.method.toLowerCase() === 'options') {
+      console.log('received an options method request');
+    var allowHeaders = ['Accept', 'Accept-Version', 'Content-Type', 'Api-Version', 'Origin', 'X-Requested-With', 'Authorization']; // added Origin & X-Requested-With & **Authorization**
 
-server.use(cors())
+    if (res.methods.indexOf('OPTIONS') === -1) res.methods.push('OPTIONS');
+
+    res.header('Access-Control-Allow-Credentials', true);
+    res.header('Access-Control-Allow-Headers', allowHeaders.join(', '));
+    res.header('Access-Control-Allow-Methods', res.methods.join(', '));
+    res.header('Access-Control-Allow-Origin', req.headers.origin);
+
+    return res.send(200);
+  }
+  else
+    return res.send(new restify.MethodNotAllowedError());
+}
+
+server.on('MethodNotAllowed', unknownMethodHandler);
+
+server.use(cors());
+
+router.configure(server);
 
 server.listen(8080, function () {
   console.log('%s listening at %s', server.name, server.url);
